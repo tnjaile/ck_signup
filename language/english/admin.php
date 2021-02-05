91fc814e3ed95cea6c9f1eb13ef3222615c4ad25
mambax7 <mambax7@gmail.com>
2021-02-05 06:04:32 -0500

    english

---
+++ language/english/admin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Ck Signup module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license    http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package    Ck Signup
+ * @since      2.5
+ * @author     ck2工作室
+ * @version    $Id $
+ **/
+
+xoops_loadLanguage('admin_common', 'tadtools');
+
+define('_MD_CKSIGNUP_ACTION_ID','Activity ID');
+define('_MD_CKSIGNUP_TITLE','Activity name');
+define('_MD_CKSIGNUP_CONTENT','Activity Description');
+define('_MD_CKSIGNUP_ACTION_DATE','Activity date');
+define('_MD_CKSIGNUP_END_DATE','Registration deadline');
+define('_MD_CKSIGNUP_UID','Publisher ID');
+define('_MD_CKSIGNUP_ENABLE','Whether to enable');
---
+++ language/english/modinfo.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Ck Signup module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license    http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package    Ck Signup
+ * @since      2.5
+ * @author     ck2工作室
+ * @version    $Id $
+ **/
+
+xoops_loadLanguage('modinfo_common', 'tadtools');
+
+define('_MI_CKSIGNUP_NAME','Activity registration');
+define('_MI_CKSIGNUP_AUTHOR','Activity registration');
+define('_MI_CKSIGNUP_CREDITS','');
+define('_MI_CKSIGNUP_DESC','The purpose of this module is to sign up for simple activities');
+define('_MI_CKSIGNUP_AUTHOR_WEB','Jill Development Website');
+define('_MI_CKSIGNUP_ADM_PAGE_0','Main Management Interface');
+define('_MI_CKSIGNUP_ADM_PAGE_0_DESC','Main management interface');
+
